959b821744912982c233eed5b496f284c64be60b
monicaakumu <moni.akumu@gmail.com>
2025-11-30 18:56:34 -0500

    Update default EC2 keypair name

--- terraform_main_ec2/variables.tf
+++ terraform_main_ec2/variables.tf
@@ -76,7 +76,7 @@ variable "instance_type" {
 variable "key_name" {
   description = "EC2 keypair"
   type        = string
-  default     = "Batch5keypair-Office-laptop"
+  default     = "KUBERNETES-KEY"
 }
 
 variable "instance_name" {
